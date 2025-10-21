289fce5ca0553eae862e69d5c7765a16093fd73d
Nasty <Selyutina31@gmail.com>
2025-10-21 22:09:39 +0700

    Класс динамический массив, реализация некоторых функций класса

---
+++ OOP/Lab2/DynamicArray.cpp
@@ -0,0 +1,148 @@
+﻿#include "DynamicArray.h"
+#include <iostream>
+
+//Конструктор по умолчанию
+DynamicArray::DynamicArray() : arrayData_(nullptr), arrayLength_(0)
+{
+    std::cout << "DynamicArray::DynamicArray()" << std::endl;
+}
+
+//Конструктор из обычного массива
+DynamicArray::DynamicArray(int initialLength) : arrayLength_(initialLength)
+{
+    std::cout << "DynamicArray::DynamicArray(int)" << std::endl;
+
+    arrayData_ = new int[initialLength];
+
+    for (int i = 0; i < arrayLength_; i++)
+    {
+        arrayData_[0];
+    }
+}
+
+//Конструктор копирования
+DynamicArray::DynamicArray(const DynamicArray& otherArray)
+{
+    std::cout << "DynamicArray::DynamicArray(const DynamicArray&)" << std::endl;
+
+    if (otherArray.arrayLength_ == 0)
+    {
+        arrayData_ = nullptr;
+        arrayLength_ = 0;
+
+        return;
+    }
+
+    arrayLength_ = otherArray.arrayLength_;
+    arrayData_ = new int[arrayLength_];
+
+    for (int index = 0; index < arrayLength_; ++index)
+    {
+        arrayData_[index] = otherArray.arrayData_[index];
+    }
+}
+
+//Конструктор перемещения
+DynamicArray::DynamicArray(DynamicArray&& otherArray)
+{
+    std::cout << "DynamicArray::DynamicArray(DynamicArray&&)" << std::endl;
+
+    arrayData_ = otherArray.arrayData_;
+    arrayLength_ = otherArray.arrayLength_;
+    otherArray.arrayData_ = nullptr;
+    otherArray.arrayLength_ = 0;
+}
+
+//Деструктор
+DynamicArray::~DynamicArray()
+{
+    std::cout << "DynamicArray::~DynamicArray()" << std::endl;
+
+    delete[] arrayData_;
+}
+
+//Вставка элемента по индексу
+bool DynamicArray::insertAt(const int index, const int value)
+{
+    if (index == 0 && arrayLength_ == 0)
+    {
+        arrayData_ = new int[1];
+
+        arrayData_[0] = value;
+
+        ++arrayLength_;
+
+        return true;
+    }
+
+    if (index < 0 || index >= arrayLength_) return false;
+
+    int* tempArrayData = new int[arrayLength_ + 1];
+
+    for (int curIdx = 0; curIdx < index; ++curIdx)
+    {
+        tempArrayData[curIdx] = arrayData_[curIdx];
+    }
+
+    for (int curIdx = index; curIdx < arrayLength_; ++curIdx)
+    {
+        tempArrayData[curIdx + 1] = arrayData_[curIdx];
+    }
+
+    tempArrayData[index] = value;
+
+    ++arrayLength_;
+
+    delete[] arrayData_;
+
+    arrayData_ = tempArrayData;
+
+    return true;
+}
+
+//Перегрузка операции присвоения копированием
+DynamicArray& DynamicArray::operator=(const DynamicArray& otherArray)
+{
+    std::cout << "DynamicArray::operator=(const DynamicArray&)" << std::endl;
+
+    if (this != &otherArray)
+    {
+        delete[] arrayData_;
+
+        if (otherArray.arrayLength_ == 0)
+        {
+            arrayData_ = nullptr;
+            arrayLength_ = 0;
+        }
+        else
+        {
+            arrayLength_ = otherArray.arrayLength_;
+            arrayData_ = new int[arrayLength_];
+
+            for (int index = 0; index < arrayLength_; ++index)
+            {
+                arrayData_[index] = otherArray.arrayData_[index];
+            }
+        }
+    }
+
+    return *this;
+}
+
+//Перегрузка операции присвоения перемещением
+DynamicArray& DynamicArray::operator=(DynamicArray&& otherArray)
+{
+    std::cout << "DynamicArray::operator=(DynamicArray&&)" << std::endl;
+
+    if (this != &otherArray)
+    {
+        delete[] arrayData_;
+
+        arrayData_ = otherArray.arrayData_;
+        arrayLength_ = otherArray.arrayLength_;
+        otherArray.arrayData_ = nullptr;
+        otherArray.arrayLength_ = 0;
+    }
+
+    return *this;
+}
---
+++ OOP/Lab2/DynamicArray.h
@@ -0,0 +1,85 @@
+﻿#pragma once
+#include <assert.h>
+
+class DynamicArray
+{
+    //- конструкторы (по умолчанию, конструктор из обычного массива, конструктор копирования) +
+    DynamicArray();
+    DynamicArray(int);
+    DynamicArray(const DynamicArray&);
+
+    //- конструктор перемещения +
+    DynamicArray(DynamicArray&&);
+
+    //- деструктор +
+    ~DynamicArray();
+
+    //- получение размера (количества хранимых элементов в настоящий момент);
+    int ArrayLen() const;
+    
+    //- обмен содержимого с другим массивом (swap);
+    void ArraySwap(DynamicArray&);
+
+    // - поиск элемента (возвращает индекс первого совпавшего элемента, либо -1, если совпадений нет);
+    int findElement(const int);
+
+    //- ввод/вывод в консоль (потоковый);
+    void input();
+    void output() const;
+    //- сортировка элементов (любым алгоритмом);
+    void ArraySort();
+
+    //- вставка элемента по индексу.Если индекс некорректный, вернуть false +
+    bool insertAt(const int, const int);
+
+    //- удаление элемента по индексу. Если индекс некорректный, вернуть false;
+    bool removeAt(const int, const int);
+    
+    //- удаление элемента по значению (первое вхождение). Если элемент отсутствует в массиве, вернуть false;
+    bool removeFirstValue(const int, const int);
+    //- удаление всех элементов с заданным значением;
+    bool removeAllValue(const int);
+
+    //- поиск максимального/минимального элемента;
+    int findMaxEl() const;
+    int findMinEl() const;
+    //- получение итераторов на начало/конец массива (методы должны называться begin и end. Метод end должен возвращать итератор не на последний элемент, а за позицию после него);
+
+    //- вставка элемента перед итератором;
+    
+    //- удаление элемента или диапазона элементов с помощью итераторов.
+
+
+    //Перегрузки:
+    
+    //- получение ссылки на элемент по индексу ([]) +
+    int& operator[](const int index)
+    {
+        assert((index >= 0 && index < arrayLength_) && "Index is out of range.");
+        return arrayData_[index];
+    }
+
+    const int& operator[](const int index) const
+    {
+        assert((index >= 0 && index < arrayLength_) && "Index is out of range.");
+        return arrayData_[index];
+    }
+
+    //-присваивание копированием(=) +
+    DynamicArray& operator=(const DynamicArray&);
+
+    //-добавление элемента в конец массива(+и +=);
+
+
+    //-присваивание перемещением(=(DynamicArray && other)) +
+    DynamicArray& operator=(DynamicArray&&);
+        
+    //- сложение(конкатенация) с другим массивом(здесь имеется в виду другим объектом нашего класса, а не стандартные массивы) (+и += );
+    //-сравнение(== и != ). 
+
+
+    private:
+
+    int* arrayData_;
+    int arrayLength_;
+};
