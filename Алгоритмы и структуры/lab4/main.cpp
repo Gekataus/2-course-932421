41b31bc28615d1b0e65f79c3f46a778b2790fcd2
Nasty <Selyutina31@gmail.com>
2025-10-29 19:56:40 +0700

    Сортировка из файла циклом

--- Алгоритмы и структуры/lab4/main.cpp
+++ Алгоритмы и структуры/lab4/main.cpp
@@ -124,11 +124,55 @@ std::vector<int> loadArrayFromFile(const std::string& filename) {
 
     return arr;
 }
+// Функция для измерения времени выполнения сортировки
+double SortingTime(vector<int> arr, const vector<int>& gaps) {
+    chrono::high_resolution_clock::time_point timeStart = chrono::high_resolution_clock::now();
+    shellSort(arr, gaps);
+    chrono::high_resolution_clock::time_point timeEnd = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = (timeEnd - timeStart);
+    return duration.count();
+}
+
+//Тестирование сортировки на трёх алгоритмах сортировки шагов
+void testArray(const vector<int>& original_arr) {
+
+    // Генерируем последовательности шагов
+    vector<int> gaps_shell = generateShell(original_arr.size());
+    vector<int> gaps_two = generateTwo(original_arr.size());
+    vector<int> gaps_knuth = generateKnuth(original_arr.size());
+
+    // Тестируем каждый метод
+    vector<int> arr1 = original_arr;
+    vector<int> arr2 = original_arr;
+    vector<int> arr3 = original_arr;
+
+    // Метод 1: Последовательность Шелла (n/2, n/4, ..., 1)
+    double time1 = SortingTime(original_arr, gaps_shell);
+    shellSort(arr1, gaps_shell);
+    bool sorted1 = isSorted(arr1);
+
+    // Метод 2: Последовательность 2^m - 1
+    double time2 = SortingTime(original_arr, gaps_two);
+    shellSort(arr2, gaps_two);
+    bool sorted2 = isSorted(arr2);
+
+    // Метод 3: Последовательность Кнута
+    double time3 = SortingTime(original_arr, gaps_knuth);
+    shellSort(arr3, gaps_knuth);
+    bool sorted3 = isSorted(arr3);
+
+    // Выводим результаты
+    cout << "Метод 1 (Шелл): " << time1 << " с, отсортирован: " << (sorted1 ? "да" : "нет") << endl;
+    cout << "Метод 2 (2^m-1): " << time2 << " с, отсортирован: " << (sorted2 ? "да" : "нет") << endl;
+    cout << "Метод 3 (Кнут): " << time3 << " с, отсортирован: " << (sorted3 ? "да" : "нет") << endl;
+    cout << endl;
+}
 int main()
 {
     setlocale(LC_ALL, "Russian");
     int x;
     vector<int> arr1;
+    vector<int> h;
     cout << "Брать массив с файла или сгенерировать вручную с консоли? Введите:" << endl << "С консоли - 1" << endl << "С файла - 2" << endl;
     cin >> x;
     if (x == 1) {
@@ -141,7 +185,6 @@ int main()
             cout << "Ваш массив:" << endl;
             outputArray(arr1);
 
-            vector<int> h;
             cout << "Как формировать шаги?" << endl << "1. Последовательность Шелла" << endl << "2. Возведение 2 в степень m" << endl << "3.Последовательность Кнута" << endl;
             cin >> x;
             if (x == 1) h = generateShell(n);
@@ -165,47 +208,12 @@ int main()
         }
     }
     if (x == 2) {
-        while (true) {
-            int y;
-            cout << "\nИз какого файла брать массив? Введите цифру" << endl;
-            cout << "1. array_10000_-10_10.txt" << endl;
-            cout << "2. array_10000_-1000_1000.txt" << endl;
-            cout << "3. array_10000_-100000_100000.txt" << endl;
-            cout << "4. array_100000_-10_10.txt" << endl;
-            cout << "5. array_100000_-1000_1000.txt" << endl;
-            cout << "6. array_100000_-100000_100000.txt" << endl;
-            cout << "7. array_1000000_-10_10.txt" << endl;
-            cout << "8. array_1000000_-1000_1000.txt" << endl;
-            cout << "9. array_1000000_-100000_100000.txt" << endl;
-            cout << "Если хотите прекратить, введите -1" << endl;
-            cin >> y;
-            if (y == -1) break;
-            if (y == 1) arr1 = loadArrayFromFile("array_10000_-10_10.txt");
-            if (y == 2) arr1 = loadArrayFromFile("array_10000_-1000_1000.txt");
-            if (y == 3) arr1 = loadArrayFromFile("array_10000_-100000_100000.txt");
-            if (y == 4) arr1 = loadArrayFromFile("array_100000_-10_10.txt");
-            if (y == 5) arr1 = loadArrayFromFile("array_100000_-1000_1000.txt");
-            if (y == 6) arr1 = loadArrayFromFile("array_100000_-100000_100000.txt");
-            if (y == 7) arr1 = loadArrayFromFile("array_1000000_-10_10.txt");
-            if (y == 8) arr1 = loadArrayFromFile("array_1000000_-1000_1000.txt");
-            if (y == 9) arr1 = loadArrayFromFile(" array_1000000_-100000_100000.txt");
-
-            vector<int> h;
-            cout << "Как формировать шаги?" << endl << "1. Последовательность Шелла" << endl << "2. Возведение 2 в степень m" << endl << "3.Последовательность Кнута" << endl;
-            cin >> x;
-            if (x == 1) h = generateShell(arr1.size());
-            if (x == 2) h = generateTwo(arr1.size());
-            if (x == 3) h = generateKnuth(arr1.size());
-
-            std::chrono::high_resolution_clock::time_point timeStart = std::chrono::high_resolution_clock::now();
-            shellSort(arr1, h);
-            std::chrono::high_resolution_clock::time_point timeEnd = std::chrono::high_resolution_clock::now();
-            std::chrono::duration<double> duration = timeEnd - timeStart;
-            if (isSorted(arr1)) {
-                cout << "Массив отсортирован" << endl << "Время сортировки: " << duration.count() << " с." << std::endl;
-            }
-
+        vector<string> filenames = { "array_10000_-10_10.txt", "array_10000_-1000_1000.txt", "array_10000_-100000_100000.txt", "array_100000_-10_10.txt", "array_100000_-1000_1000.txt", "array_100000_-100000_100000.txt", "array_1000000_-10_10.txt", "array_1000000_-1000_1000.txt", "array_1000000_-100000_100000.txt"};
+        for (int i = 0; i < filenames.size(); i++) {
+            string filename = filenames[i];
+            cout << "Сортировка массива из файла " << filename << endl;
+            vector<int> arr = loadArrayFromFile(filename);
+            testArray(arr);
         }
-
     }
 }
