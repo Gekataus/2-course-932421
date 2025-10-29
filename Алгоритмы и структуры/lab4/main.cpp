b9f473af3497f6061d5dd88b0813a947d8afe2f7
Nasty <Selyutina31@gmail.com>
2025-10-29 10:19:59 +0700

    Формирование шагов тремя методами, ввод массива с консоли

--- Алгоритмы и структуры/lab4/main.cpp
+++ Алгоритмы и структуры/lab4/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <cmath>
 using namespace std;
 
 //создание массива случайных чисел (Библиотека random)
@@ -34,31 +35,108 @@ bool isSorted(vector<int>& arr) {
 	return true;
 }
 
-// Сортировка Шелла с последовательностью Шелла
-void shellSort(vector<int>& arr) {
-	int n = arr.size();
-	//Изменяем шаги
-	for (int gap = n / 2; gap > 0; gap /= 2) {
-		// Выполняем сортировку вставками для этого шага
-		for (int i = gap; i < n; i++) {
-			int temp = arr[i];
-			int j;
-			// Сдвигаем элементы, пока не найдем правильную позицию для arr[i]
-			for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
-				arr[j] = arr[j - gap];
-			}
-			arr[j] = temp;
-		}
-	}
+// 1. Последовательность Шелла (h[0]0 = n/2, h[i] = h[i-1]/2, ..., h[t] = 1)
+vector<int> generateShell(int n) {
+    vector<int> gaps;
+    int h = n / 2;
+
+    while (h > 0) {
+        gaps.push_back(h);
+        h /= 2;
+    }
+
+    return gaps;
 }
-int main() 
+
+// 2. Последовательность вида h[m] = 2^m - 1 при m = 1, 2, ..., t = log2(n)
+vector<int> generateTwo(int n) {
+    vector<int> gaps;
+    int t = log2(n);
+
+    // m = 1, 2, ..., t (в порядке убывания)
+    for (int m = t; m >= 1; m--) {
+        int h_m = pow(2, m) - 1;
+        gaps.push_back(h_m);
+    }
+
+    return gaps;
+}
+
+// 3. Последовательность Кнута: h[i] = 1, h[j+1] = 3h[j] + 1 для j = 1, 2, ..., t-1
+vector<int> generateKnuth(int n) {
+    vector<int> gaps;
+
+    // Начинаем с h[i] = 1
+    int h_j = 1;
+    gaps.push_back(h_j);
+
+    // Вычисляем h[j+1] = 3h[j] + 1 пока h[j+1] < n
+    // j = 1, 2, ..., t-1 (t - количество элементов в последовательности)
+    while (true) {
+        int h_j_plus_1 = 3 * h_j + 1;  // h[j+1] = 3h[j] + 1
+        if (h_j_plus_1 >= n) {
+            break;
+        }
+        gaps.push_back(h_j_plus_1);
+        h_j = h_j_plus_1;
+    }
+
+    // Реверсируем для использования от большего к меньшему
+    reverse(gaps.begin(), gaps.end());
+
+    return gaps;
+}
+
+//Сортировка Шелла
+void shellSort(vector<int>& arr, const vector<int>& gaps) {
+    int n = arr.size();
+
+    for (int gap : gaps) {
+        // Сортировка вставками с данным шагом
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j = i;
+
+            // Сдвигаем элементы, пока не найдем правильную позицию
+            while (j >= gap && arr[j - gap] > temp) {
+                arr[j] = arr[j - gap];
+                j -= gap;
+            }
+            arr[j] = temp;
+        }
+    }
+}
+int main()
 {
-	vector<int> arr1 = generateRandomArray(20, 0, 99);
-	outputArray(arr1);
+    setlocale(LC_ALL, "Russian");
+    int x;
+    vector<int> arr1;
+    cout << "Брать массив с файла или сгенерировать вручную с консоли?" << endl << "С консоли - 1" << endl << "С файла - 2" << endl;
+    cin >> x;
+    if (x == 1)
+    {
+        int n, min, max;
+        cout << "Введите длинну массива, минимальную и максимальную границу" << endl;
+        cin >> n >> min >> max;
+        vector<int> arr1 = generateRandomArray(n, min, max);
+        cout << "Ваш массив:" << endl;
+        outputArray(arr1);
 
-	shellSort(arr1);
-	outputArray(arr1);
+        vector<int> h;
+        cout << "Как формировать шаги?" << endl << "1. Последовательность Шелла" << endl << "2. Возведение 2 в степень m" << endl << "3.Последовательность Кнута" << endl;
+        cin >> x;
+        if (x == 1) h = generateShell(n);
+        if (x == 2) h = generateTwo(n);
+        if (x == 3) h = generateKnuth(n);
+        shellSort(arr1, h);
 
-	
-
+        if (isSorted(arr1))
+        {
+            cout << "Массив отсортирован" << endl;
+            if (n < 100) {
+                cout << "Отсортированный массив:" << endl;
+                outputArray(arr1);
+            }
+        }
+    }
 }
