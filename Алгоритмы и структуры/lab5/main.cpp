ff047141a72c0927e900ee661afef492b180df26
Nasty <Selyutina31@gmail.com>
2025-11-11 21:26:38 +0700

    функции для работы с массивами

---
+++ Алгоритмы и структуры/lab5/main.cpp
@@ -0,0 +1,60 @@
+﻿#include <vector>
+#include <random>
+#include <fstream>
+#include <chrono>
+#include <iostream>
+using namespace std;
+
+//создание массива случайных чисел (Библиотека random)
+vector<int> generateRandomArray(int size, int min_val, int max_val) {
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<int> distrib(min_val, max_val);
+
+	vector<int> arr(size);
+	for (int i = 0; i < size; i++) {
+		arr[i] = distrib(gen);
+	}
+	return arr;
+}
+
+// Вывод массива
+void outputArray(const vector<int>& arr) {
+	for (int num : arr) {
+		cout << num << " ";
+	}
+	cout << endl;
+}
+
+// Проверка на отсортированность
+bool isSorted(vector<int>& arr) {
+	for (int i = 1; i < arr.size(); i++) {
+		if (arr[i] < arr[i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// Загрузка массива из файла
+std::vector<int> loadArrayFromFile(const std::string& filename) {
+	std::vector<int> arr;
+	std::ifstream file(filename);
+	int value;
+
+	if (file.is_open()) {
+		while (file >> value) {
+			arr.push_back(value);
+		}
+		file.close();
+	}
+
+	return arr;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+
+}
