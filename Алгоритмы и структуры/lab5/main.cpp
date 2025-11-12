4c5da01d1070ba014dc18b70754e21a64483c6b8
Nasty <Selyutina31@gmail.com>
2025-11-12 17:45:05 +0700

    Сортировка рандомного массива, сортировка из файлов

--- Алгоритмы и структуры/lab5/main.cpp
+++ Алгоритмы и структуры/lab5/main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <chrono>
 #include <iostream>
+#include <string>
 using namespace std;
 
 //создание массива случайных чисел (Библиотека random)
@@ -52,54 +53,81 @@ std::vector<int> loadArrayFromFile(const std::string& filename) {
 	return arr;
 }
 
-void HoarSort(vector<int>& arr, int l, int r, int xl, int xr) {
-	if (l >= r || xl == xr) return;
+// Функция быстрой сортировки (алгоритм Хоара)
+void hoarSort(vector<int>& arr, int l, int r) {
+	if (l >= r) return;
+	int x = arr[(l + r) / 2];
 	int i = l, j = r;
-	float xm = (xl + xr) / 2.0f;
 	while (i <= j) {
-		// Ищем элемент слева, который >= xm
-		while (i <= j && arr[i] < xm) i++;
-		// Ищем элемент справа, который < xm
-		while (j >= i && arr[j] >= xm) j--;
-		// Если нашли пару для обмена
-		if (i < j) {
+		while (arr[i] < x) i++;
+		while (arr[j] > x) j--;
+		if (i <= j) {
 			swap(arr[i], arr[j]);
 			i++;
 			j--;
 		}
 	}
-	// от xl до xm
-	if (l <= j) {
-		HoarSort(arr, l, j, xl, (int)xm);
-	}
-	// от xm до xr
-	if (i <= r) {
-		HoarSort(arr, i, r, (int)(xm + 0.5), xr);
-	}
+	if (l < j) hoarSort(arr, l, j);
+	if (i < r) hoarSort(arr, i, r);
 }
 
+// Функция для измерения времени выполнения сортировки
+double SortingTime(vector<int>& arr, int l, int r) {
+	chrono::high_resolution_clock::time_point timeStart = chrono::high_resolution_clock::now();
+	hoarSort(arr, l, r);
+	chrono::high_resolution_clock::time_point timeEnd = chrono::high_resolution_clock::now();
+	chrono::duration<double> duration = (timeEnd - timeStart);
+	return duration.count();
+}
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	int n, min, max;
-	cin >> n >> min >> max;
-	vector<int> arr1 = generateRandomArray(n, min, max);
-	
+	double time = 0;
 	vector<string> filenames = {
-		"array_10000_-10_10",
-		"array_10000_-1000_1000",
-		"array_10000_-100000_100000",
-		"array_100000_-10_10",
-		"array_100000_-1000_1000",
-		"array_100000_-100000_100000",
-		"array_1000000_-10_10",
-		"array_1000000_-1000_1000",
-		"array_1000000_-100000_100000"
+		"array_10000_-10_10.txt",
+		"array_10000_-1000_1000.txt",
+		"array_10000_-100000_100000.txt",
+		"array_100000_-10_10.txt",
+		"array_100000_-1000_1000.txt",
+		"array_100000_-100000_100000.txt",
+		"array_1000000_-10_10.txt",
+		"array_1000000_-1000_1000.txt",
+		"array_1000000_-100000_100000.txt"
 	};
-	for (int i = 0; i < filenames.size(); i++) {
-		string filename = filenames[i];
 
+	int x = 0;
+	for (;;) {
+		cout << "Введите цифру:" << endl << "1.Сортировать рандомный массив" << endl << "2.Сортировать из файлов" << endl << "-1 - Прекратить сортировку" << endl;
+		cin >> x;
+		if (x == -1) exit;
+		
+		if (x == 1) {
+			cout << "Введите размерность массива, минимальное и максимальное значение" << endl;
+			cin >> n >> min >> max;
+			vector<int> arrsort = generateRandomArray(n, min, max);
+			cout << "Ваш массив: " << endl; outputArray(arrsort);
+			cout << "\nСортировка массива..." << endl;
+			time = SortingTime(arrsort, 0, n - 1);
+			cout << "Массив отсортирован?: " << (isSorted(arrsort) ? "Да" : "Нет") << endl;
+			cout << "Время сортировки: " << time << endl << endl;
+			cout << "Отсортированный массив: " << endl; outputArray(arrsort);
+			cout << endl << endl;
+		}
+		
+		if (x == 2) {
+			for (int i = 0; i < filenames.size(); i++) {
+				string filename = filenames[i];
+				vector<int> arr = loadArrayFromFile(filename);
+				n = arr.size();
+				cout << "Сортировка массива из файла " << filename << endl;
+				time = SortingTime(arr, 0, n - 1);
+				cout << "Массив отсортирован?: " << (isSorted(arr) ? "Да" : "Нет") << endl;
+				cout << "Время сортировки: " << time << endl << endl;
+			}
+		}
 	}
+
 	return 0;
 }
