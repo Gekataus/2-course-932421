9bada61703d5d2ddddc18658f137d2c3d986d578
Nasty <Selyutina31@gmail.com>
2025-11-25 22:39:28 +0700

    Сортировка с отрицательными числами, сортировка массивов из файлов

--- Алгоритмы и структуры/lab6_bitwise_sorting/main.cpp
+++ Алгоритмы и структуры/lab6_bitwise_sorting/main.cpp
@@ -4,23 +4,8 @@
 #include <chrono>
 #include <iostream>
 #include <string>
-#include <cmath>
 
 using namespace std;
-
-int getHighestBitPosition(int num) {
-	if (num == 0) return -1; // нет установленных битов
-
-	unsigned int unsigned_num = static_cast<unsigned int>(num < 0 ? -num : num);
-	int position = 0;
-
-	while (unsigned_num > 1) {
-		unsigned_num >>= 1;
-		position++;
-	}
-
-	return position;
-}
 
 //создание массива случайных чисел (Библиотека random)
 vector<int> generateRandomArray(int size, int min_val, int max_val) {
@@ -69,53 +54,89 @@ vector<int> loadArrayFromFile(const string& filename) {
 	return arr;
 }
 
-// Функция для получения k-го бита числа
+// Получение k-го бита числа
 int getBit(int num, int k) {
-	return (num >> k) & 1;
+	unsigned int unsigned_num = static_cast<unsigned int>(num);
+	return (unsigned_num >> k) & 1;
 }
 
 // Рекурсивная функция побитовой сортировки
 void bitwiseSort(vector<int>& arr, int l, int r, int k) {
-	// 1.Если l ≥ r или k < 0, то выход
 	if (l >= r || k < 0) {
 		return;
 	}
 
-	// 2. Установка начальных значений
 	int i = l;
 	int j = r;
 
-	// 3. Разделение элементов по k-му биту
-	while (i <= j) {
-		// 3.1 Ищем элемент с 0 в k-м разряде слева
-		while (i <= j && getBit(arr[i], k) == 0) {
-			i++;
+	// Разделяем отрицательные и положительные числа (отриц - влево, положит - вправо)
+	if (k == sizeof(int) * 8 - 1) {
+		// отрицательное число - бит = 1
+		// положительное число - бит = 0
+		while (i <= j) {
+			while (i <= j && getBit(arr[i], k) == 1) { // отрицательные
+				i++;
+			}
+			while (i <= j && getBit(arr[j], k) == 0) { // положительные
+				j--;
+			}
+			if (i < j) {
+				std::swap(arr[i], arr[j]);
+				i++;
+				j--;
+			}
 		}
+	}
+	else {
+		// Для остальных битов - обычный алгоритм
+		while (i <= j) {
+			while (i <= j && getBit(arr[i], k) == 0) {
+				i++;
+			}
 
-		// 3.2 Ищем элемент с 1 в k-м разряде справа
-		while (i <= j && getBit(arr[j], k) == 1) {
-			j--;
-		}
+			while (i <= j && getBit(arr[j], k) == 1) {
+				j--;
+			}
 
-		// 3.3 Меняем местами элементы, если нужно
-		if (i < j) {
-			std::swap(arr[i], arr[j]);
-			i++;
-			j--;
+			if (i < j) {
+				std::swap(arr[i], arr[j]);
+				i++;
+				j--;
+			}
 		}
 	}
 
-	// 4. Выполняем эту же процедуру для фрагмента [l, j] по k - 1 разряду
+	// Рекурсивные вызовы
 	bitwiseSort(arr, l, j, k - 1);
-
-	// 5. Выполняем эту же процедуру для фрагмента [i, r] по k - 1 разряду
 	bitwiseSort(arr, i, r, k - 1);
+}
+
+// Функция для измерения времени выполнения сортировки
+double SortingTime(vector<int>& arr, int l, int r, int k) {
+	chrono::high_resolution_clock::time_point timeStart = chrono::high_resolution_clock::now();
+	bitwiseSort(arr, l, r, k);
+	chrono::high_resolution_clock::time_point timeEnd = chrono::high_resolution_clock::now();
+	chrono::duration<double> duration = (timeEnd - timeStart);
+	return duration.count();
 }
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
+	int max_bit = sizeof(int) * 8 - 1;
 	int x, n, min_val, max_val;
+	double time = 0;
+	vector<string> filenames = {
+		"array_10000_-10_10.txt",
+		"array_10000_-1000_1000.txt",
+		"array_10000_-100000_100000.txt",
+		"array_100000_-10_10.txt",
+		"array_100000_-1000_1000.txt",
+		"array_100000_-100000_100000.txt",
+		"array_1000000_-10_10.txt",
+		"array_1000000_-1000_1000.txt",
+		"array_1000000_-100000_100000.txt"
+	};
 	for (;;) {
 		cout << "Над каким массивом выполнять сортировку? Введите цифру: " << endl;
 		cout << "1. Создать рандомный массив" << endl;
@@ -131,15 +152,22 @@ int main()
 			arr1 = generateRandomArray(n, min_val, max_val);
 			cout << "Ваш массив:" << endl;
 			outputArray(arr1);
-			bitwiseSort(arr1, 0, n - 1, getHighestBitPosition(max_val));
+			bitwiseSort(arr1, 0, n - 1, max_bit);
 			cout << "Отсортированный массив: " << endl;
 			outputArray(arr1);
 			cout << "Массив отсортирован?: " << (isSorted(arr1) ? "Да" : "Нет") << endl << endl;
 		}
+		if (x == 2) {
+			for (int i = 0; i < filenames.size(); i++) {
+				string filename = filenames[i];
+				vector<int> arr = loadArrayFromFile(filename);
+				n = arr.size();
+				cout << "Сортировка массива из файла " << filename << endl;
+				time = SortingTime(arr, 0, n - 1, max_bit);
+				cout << "Массив отсортирован?: " << (isSorted(arr) ? "Да" : "Нет") << endl;
+				cout << "Время сортировки: " << time << endl << endl;
+			}
+		}
 	}
-
-
-
-
 	return 0;
 }
