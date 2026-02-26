53e0dda9f3795b50b5d6b0fdc65803bc2605a49b
Nasty <Selyutina31@gmail.com>
2026-02-26 11:32:39 +0700

    Улучшена проверка на отсортированность массива в файле, функция разбиения массивов

--- Алгоритмы и структуры 4 семестр/lab1_FileMergeSort/FileMerge.cpp
+++ Алгоритмы и структуры 4 семестр/lab1_FileMergeSort/FileMerge.cpp
@@ -1,40 +1,75 @@
 ﻿#include <random>
 #include <fstream>
 #include <iostream>
+#include <algorithm>
 
 using namespace std;
 
 
 //Проверка файла на упорядоченность
-bool isFileContainsSortedArray(const std::string& fileName)
+int isFileContainsSortedArray(const std::string& fileName)
 {
 	std::fstream outputFile(fileName);
+	if (!outputFile.is_open()) {
+		std::cout << "Error: couldn't read from file in function 'isFileContainsSortedArray." << std::endl;
+		return -1;
+	}
 	int value1 = 0, value2 = 0;
 
 	outputFile >> value1;
 	while (outputFile >> value2) {
 		if (value1 > value2)
-			return false;
+			return 0;
 		value1 = value2;
 	}
-	return true;
+	return 1;
 };
+
+//Разбиение
+void split(const std::string& readingFile, const std::string& firstFile, const std::string& secondFile, const int p) {
+	std::ifstream outputFile(readingFile);
+	std::ofstream fileA(firstFile), fileB(secondFile);
+	
+	if (!outputFile.is_open() || !fileA.is_open() || !fileB.is_open()) {
+		std::cout << "Error: couldn't read from files in function 'split'." << std::endl;
+		return;
+	}
+	int value;
+	bool writeToA = true;
+
+	while (outputFile >> value) {
+		for (int i = 0; i < p && outputFile; i++) {
+			if (writeToA) fileA << value << " ";
+			else fileB << value << " ";
+			writeToA = !writeToA;
+			if (i < p - 1)
+				if (!(outputFile >> value)) 
+					break;
+		}
+	}
+}
+
+//Слияние
+void merge(const std::string& readingFile1, const std::string& readingFile2, const std::string& writingFile1, const std::string& writingFile2, const int p) {
+	std::ifstream file1(readingFile1), file2(readingFile2);
+	std::ofstream file3(writingFile1), file4(writingFile2);
+
+	int value1, value2;
+	while (file1 >> value1 && file2 >> value2) {
+		std::vector<int> mas;
+		for (int i = 0; i < p; i++)
+		{
+			mas.push_back(value1);
+			mas.push_back(value2);
+		}
+		sort(mas.begin(), mas.end());
+
+	}
+}
 
 //Прямая сортировка
 void StraightMergeSort(const std::string& fileName)
 {
-	std::fstream f(fileName, ios::out);
-	std::fstream fa("fa.txt");
-	std::fstream fb("fb.txt");
-	std::fstream fc("fc.txt");
-	std::fstream fd("fd.txt");
-	int p = 1;
-	int value = 0;
-	while(f >> value)
-	for (int i = 0; i < p; i++)
-	{
-
-	}
 };
 
 //Естественная сортировка
